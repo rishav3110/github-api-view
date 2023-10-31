bd59f5367122d95b5610d1c407576f35be329e4a
Rishav <132085385+rishav3110@users.noreply.github.com>
2023-11-01 03:04:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
 const APIURL = "https://api.github.com/users/";
 const main = document.querySelector("#main");
+const searchBox = document.querySelector("#search");
 const getUser = async(username) => {
     const response = await fetch(APIURL + username);
-    const data = await response.json()
+    const data = await response.json();
     console.log(data);
-    const card = `
-    <div class="card">
-    <div id="imgbox">
-                <img src="${data.avatar_url}" alt="" class="avatar">
-            </div>
-            <div class="userinfo">
-                <h2>${data.name}</h2>
-                <p>Bio</p>
-                <ul class="info">
-                    <li>Followers</li>
-                    <li>Following</li>
-                    <li>Repost</li>
-                </ul>
-            </div>
-            <div class="Repost">
-                <a class="repo" href="#" target="_blank">Repo1</a>
-                <a class="repo" href="#" target="_blank">Repo2</a>
-                <a class="repo" href="#" target="_blank">Repo3</a>
-            </div>
-        </div>
-        `
-        main.innerHTML = card;
+    document.getElementById('avatar').src=`${data.avatar_url}`
+document.getElementById('username').innerHTML=`${data.login}`
+document.getElementById('bio').innerHTML=`${data.bio}`
+document.getElementById('name').innerHTML=`${data.name}`
+document.getElementById('location').innerHTML=`${data.location}`
+document.getElementById('followers').innerHTML=`${data.followers}`
+document.getElementById('following').innerHTML=`${data.following}`
 }
+    const formSubmit = () => {
+        if(searchBox.value !==""){
+            getUser(searchBox.value);
+        }
+        return false;
+    };
